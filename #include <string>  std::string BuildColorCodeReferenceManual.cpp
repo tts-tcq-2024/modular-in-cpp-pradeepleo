5bfa11cadbfe742ba3420ee157607592a22dc334
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-10-13 09:33:35 +0530

    Create #include <string>  std::string BuildColorCodeReferenceManual.cpp

---
+++ #include <string>  std::string BuildColorCodeReferenceManual.cpp
@@ -0,0 +1,13 @@
+std::string BuildColorCodeReferenceManual() {
+  std::string referenceManual;
+  referenceManual = "\nCode\tMajor Color\tMinor Color";
+  for(int pairNumber = 1; pairNumber <= 25; ++pairNumber) {
+    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    referenceManual += "\n"; 
+    referenceManual += std::to_string(pairNumber);
+    referenceManual += "\t";
+    referenceManual += colorPair.ToString();
+  }
+
+  return referenceManual;
+}
