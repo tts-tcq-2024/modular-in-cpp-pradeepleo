f190a6416853c539c3f4be9a30fa3deac52cc1e9
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-10-13 09:48:46 +0530

    Update BuildColorCodeReferenceManual.cpp

--- BuildColorCodeReferenceManual.cpp
+++ BuildColorCodeReferenceManual.cpp
@@ -5,7 +5,7 @@ std::string BuildColorCodeReferenceManual() {
   std::string referenceManual;
   referenceManual = "\nCode\tMajor Color\tMinor Color";
   for(int pairNumber = 1; pairNumber <= 25; ++pairNumber) {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    TelCoColorCoder::ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     referenceManual += "\n"; 
     referenceManual += std::to_string(pairNumber);
     referenceManual += "\t";
