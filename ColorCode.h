e4f22ce839966736fa50505897fc108c9b487005
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-10-13 09:56:19 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -8,8 +8,8 @@ namespace TelCoColorCoder {
     enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
     enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
-    const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
-    const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    extern const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    extern const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
     int const numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
     int const numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
