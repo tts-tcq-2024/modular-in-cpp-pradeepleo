e50e58f10fb26b498dfdbed047b215a09d21bc58
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-10-13 09:21:28 +0530

    Create ColorCode.h

---
+++ ColorCode.h
@@ -0,0 +1,29 @@
+#ifndef COLORCODE_H
+#define COLORCODE_H
+
+#include <string>
+
+namespace TelCoColorCoder {
+
+    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+    enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+    const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+    int const numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    int const numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    class ColorPair {
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+    public:
+        ColorPair(MajorColor major, MinorColor minor);
+        MajorColor getMajor();
+        MinorColor getMinor();
+        std::string ToString();
+    };
+
+}
+#endif
