9cbb14617204d6b140d9db039754df83658bd8cd
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-10-13 09:57:08 +0530

    Update ColorCode.cpp

--- ColorCode.cpp
+++ ColorCode.cpp
@@ -1,7 +1,8 @@
 #include "ColorCode.h"
 
 namespace TelCoColorCoder {
-
+    const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
     ColorPair::ColorPair(MajorColor major, MinorColor minor) : majorColor(major), minorColor(minor) {}
 
     MajorColor ColorPair::getMajor() {
